407537eb2d4c08bfd1551be7c1714f5dee5bc047
Christy Guan <cguan00@stuy.edu>
2019-02-26 21:23:06 -0500

    completed printing out 2D array of chars

--- ReadFile.java
+++ ReadFile.java
@@ -1,6 +1,8 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
+import java.util.*;
+import java.io.*;
 
 public class ReadFile {
   public static void main(String args[]) throws FileNotFoundException {
@@ -8,29 +10,42 @@ public class ReadFile {
 
     //inf stands for the input file
     Scanner inf = new Scanner(text);
-    int numLines = 0;
 
+    //store each line of the text file
+    ArrayList<String> lines = new ArrayList<String>();
+
+    //print out maze by reading file line by line
     while(inf.hasNextLine()){
       String line = inf.nextLine();
-      numLines += 1;
-      System.out.println(line);//hopefully you can do other things with the line
+      System.out.println(line);
+      lines.add(line);
     }
 
-    String[] lines = new String[numLines];
+    // System.out.println(lines);
+    System.out.println();
+    System.out.println();
 
-    while(inf.hasNextLine()){
-      for(int i = 0; i < numLines; i++){
-        String line = inf.nextLine();
-        lines[i] = line;
+    int numLines = lines.size();//number of "rows"
+    int numCharsPerLine = lines.get(0).length();//number of "cols"
+    String[][] chars = new String[numLines][numCharsPerLine];//created 2D array
+
+    //loop through each character of each line and add it to the 2D array
+    for(int i = 0; i < numLines; i++){
+      for(int j = 0; j < numCharsPerLine; j++){
+        // System.out.println(lines.get(i).charAt(j));
+        chars[i][j] = lines.get(i).charAt(j) + "";
       }
     }
 
-    for(int i = 0; i < numLines; i++){
-      System.out.println(lines[i]);
-      System.out.println("");
+    //like a toString to create string of 2D array that will be printed
+    String output = "";
+    for(int i = 0; i < chars.length; i++){
+      for(int j = 0; j < chars[i].length; j++){
+        output += chars[i][j];
+      }
+      output += "\n";
     }
 
-
-    System.out.println(numLines);
+    System.out.println(output);//print out maze by reading 2D array of characters
   }
 }
