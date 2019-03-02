ad9ed59f4eb384a673d1bae968da5ed0b554ccb7
Christy Guan <cguan00@stuy.edu>
2019-03-02 14:40:57 -0500

    tested three mazes and commented out animation

--- Driver.java
+++ Driver.java
@@ -3,18 +3,29 @@ import java.io.*;
 
 public class Driver{
     public static void main(String[]args){
-      String file1 = "data1.dat";
-      String file2 = "data2.dat";
-      String file3 = "data3.dat";
       try{
-        Maze f;
-        f = new Maze(file1);//true animates the maze.
+        String file1 = "data1.dat";
+        Maze maze1 = new Maze(file1);
+        System.out.println(maze1);
+        // maze1.setAnimate(true);
+        maze1.solve();
+        System.out.println(maze1);
 
-        // f.setAnimate(true);
-        //f.solve();
-        System.out.println(f);
+        String file2 = "data2.dat";
+        Maze maze2 = new Maze(file2);
+        System.out.println(maze2);
+        // maze2.setAnimate(true);
+        maze2.solve();
+        System.out.println(maze2);
+
+        String file3 = "data3.dat";
+        Maze maze3 = new Maze(file3);
+        System.out.println(maze3);
+        // maze3.setAnimate(true);
+        maze3.solve();
+        System.out.println(maze3);
       }catch(FileNotFoundException e){
-        System.out.println("Invalid filename: "+ file1);
+        System.out.println("Invalid filename");
       }catch(IllegalStateException e){
         System.out.println("Not a valid file");
       }
